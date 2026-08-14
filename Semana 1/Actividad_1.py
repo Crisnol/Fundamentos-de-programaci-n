2f1cedcc11ceed4a611c5893fcb6566b6e20e50f
Cristian Nolasco <cnolasco082@gmail.com>
2026-08-14 08:55:18 -0600

    Guardado del archivo principal actividad 1 calculadora de tiempo digital

---
+++ Semana 1/Actividad_1.py
@@ -0,0 +1,17 @@
+##CALCULADORA DE TIEMPO DIGITAL
+
+userName = input("Bienvenido ingrese su nombre: ")
+
+timeYoutube = float(input("Ingrese el tiempo ocupado en Youtube (hrs): "))
+timeInstagram = float(input("Ingrese el tiempo ocupado en Instagram (hrs): "))
+timeWhatsApp = float(input("Ingrese el tiempo ocupado en WhatsApp (hrs): "))
+timeSpotify = float(input("Ingrese el tiempo ocupado en Spotify (hrs): "))
+timeNetflix = float(input("Ingrese el tiempo ocupado en Netflix (hrs): "))
+timeGames = float(input("Ingrese el tiempo ocupado en Videojuegos (hrs): "))
+
+totalTime = timeYoutube + timeInstagram + timeWhatsApp + timeSpotify + timeNetflix + timeGames
+pocentaje = (totalTime/24)*100
+
+print(f"\n{userName}, Este es tu resumen diario del tiempo en diferentes plataformas")
+print(f"Tiempo total acumulado: {totalTime} hrs")
+print(f"Porcentaje de tu dia: {pocentaje}%\n")
