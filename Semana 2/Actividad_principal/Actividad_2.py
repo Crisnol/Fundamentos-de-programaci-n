0f044ed061ad85434a5c8103d667d2aa8128175a
Cristian Nolasco <cnolasco082@gmail.com>
2026-08-19 11:40:26 -0600

    Aprobacion del sistema, solo falta el formateo de la salida

--- Semana 2/Actividad_principal/Actividad_2.py
+++ Semana 2/Actividad_principal/Actividad_2.py
@@ -15,17 +15,22 @@ DESCUENTO_ESTUDIANTE = 0.1
 
 num_visitantes = int(input("Ingresa el número de visitantes:  "))
 i = 1
+total_general = 0
 
 while (i <= num_visitantes):
-    edad = int(input("Ingrese la edad del visitante: "))
-    tipo_visitante = input("Ingresa el tipo de visitante (Adulto mayor / Profesor / Estudiante): ")
+    # pdb.set_trace()
+
+    edad = int(input("\nIngrese la edad del visitante: "))
 
     if (edad >= 18):
         precio_boleto = PRECIO_MAYOR_18
     elif(edad <= 17 and edad >= 3):
         precio_boleto = PRECIO_MENOR_EDAD
     else:
-       continue
+        i += 1
+        continue
+    
+    tipo_visitante = input("Ingresa el tipo de visitante (Adulto mayor / Profesor / Estudiante): ")
 
     if (tipo_visitante == "Adulto mayor"):
         precio_descuento = precio_boleto * (1 - DESCUENTO_ADULTO)
@@ -35,7 +40,8 @@ while (i <= num_visitantes):
             precio_descuento = precio_boleto * (1 - DESCUENTO_ESTUDIANTE)
     else:
         precio_descuento = precio_boleto * 1         
-    
 
-
+    total_general = total_general + precio_descuento
     i += 1
+
+print(f"\nTotal a pagar con descuento: {total_general}")
